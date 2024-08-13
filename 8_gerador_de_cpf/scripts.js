5079053f6c731fb832facc4b0e062263232f51ce
Bruno Macedo <brunomacedo1203@gmail.com>
2024-08-13 20:00:21 -0300

    gerador de CPF

---
+++ 8_gerador_de_cpf/scripts.js
@@ -0,0 +1,46 @@
+const cpfEl = document.getElementById("cpf");
+const gerarCpfBtn = document.getElementById("gerar-cpf");
+const copiarCpfBtn = document.getElementById("copiar-cpf");
+
+// Função para gerar CPF aleatório
+function gerarCPF() {
+  /*Este trecho de código gera um número aleatório de 9 dígitos e garante 
+  que ele tenha  9 caracteres, completando com zeros à esquerda se necessário.*/
+    let n = Math.floor(Math.random() * 999999999) + 1;
+  let nStr = n.toString().padStart(9, "0");
+  let dv1 = calcularDV(nStr, 10);
+  let dv2 = calcularDV(nStr + dv1, 11);
+
+  cpfEl.innerText = formatarCPF(nStr + dv1 + dv2);
+}
+
+function calcularDV(numero, peso) {
+  let total = 0;
+  for (let i = 0; i < numero.length; i++) {
+    total += parseInt(numero.charAt(i)) * peso--;
+  }
+  let resto = total % 11;
+  return resto < 2 ? 0 : 11 - resto;
+}
+
+// A função a seguir vai formatar o cpf para ficar no padrão de apresentação que a gente conhece
+function formatarCPF(cpf) {
+  const cpfRegex = /^(\d{3})(\d{3})(\d{3})(\d{2})$/;
+  return cpf.replace(cpfRegex, "$1.$2.$3-$4");
+}
+
+// Função para copiar CPF para área de transferência
+function copiarCPF() {
+  const cpf = cpfEl.innerText;
+  navigator.clipboard.writeText(cpf).then(
+    () => {
+      alert(`CPF ${cpf} copiado para a área de transferência!`);
+    },
+    (err) => {
+      console.error("Erro ao copiar CPF: ", err);
+    }
+  );
+}
+
+gerarCpfBtn.addEventListener("click", gerarCPF);
+copiarCpfBtn.addEventListener("click", copiarCPF);
